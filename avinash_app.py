626a8fc4743cb97234a42aceb2a1d7918b23e695
prathamodasia <modasiapratha@gmail.com>
2024-07-05 10:16:52 +0530

    Update avinash_app.py

--- avinash_app.py
+++ avinash_app.py
@@ -31,4 +31,4 @@ if st.button("Seles Prediction "):
     result = clf.predict(np.array([[tv,rd,newspaper]]))
     st.text(result[0])
 
-st.markdown("Developed By Meet Patel at NIELIT Daman")
+st.markdown("Developed By Pratha Modasia at NIELIT Daman")
